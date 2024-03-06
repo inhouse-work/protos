19329e209f33d5b7de89f3ec640234577177c6c7
Nolan J Tait <nolanjtait@gmail.com>
2024-03-06 14:32:29 -0800

    Fixes dropdown specs

--- spec/protos/dropdown/menu_spec.rb
+++ spec/protos/dropdown/menu_spec.rb
@@ -5,13 +5,7 @@ RSpec.describe Protos::Dropdown::Menu do
     render described_class.new { "menu" }
   end
 
-  it "renders the menu" do
-    expect(page).to have_css("ul")
-    expect(page).to have_content("menu")
-  end
-
-  it "renders the styles" do
-    expect(page).to have_css(".menu")
-    expect(page).to have_css(".dropdown-content")
+  it "renders the template" do
+    expect(page).to have_css("template", visible: :hidden)
   end
 end
--- spec/protos/dropdown_spec.rb
+++ spec/protos/dropdown_spec.rb
@@ -2,17 +2,29 @@
 
 RSpec.describe Protos::Dropdown do
   before do
-    render described_class.new(hover: true) { "Hello, World!" }
+    render described_class.new(
+      position: :top,
+      animation: :shift_away,
+      duration: [200, 200],
+      hide_on_click: :toggle,
+      z_index: 10
+    )
   end
 
-  it "renders the dropdown" do
-    expect(page).to have_css("details")
-    expect(page).to have_content("Hello, World!")
+  it "renders the popover" do
+    expect(page).to have_css("div")
   end
 
-  it "renders the styles" do
-    expect(page).to have_css(".dropdown-bottom")
-    expect(page).to have_css(".dropdown")
-    expect(page).to have_css(".dropdown-hover")
+  it "connects the stimulus controller" do
+    options = JSON.generate({
+      animation: "shift-away",
+      placement: "top",
+      duration: [200, 200],
+      hideOnClick: "toggle",
+      zIndex: 10
+    })
+
+    expect(page).to have_css("div[data-controller='protos--popover']")
+    expect(page).to have_css("div[data-protos--popover-options-value='#{options}']")
   end
 end

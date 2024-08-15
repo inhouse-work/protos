9276dcfe541ea17b5c64ce06c7309aa14d6b904c
Nolan J Tait <nolanjtait@gmail.com>
2024-08-14 20:57:32 -0700

    Removes autoloading in engine, not required

--- lib/protos/engine.rb
+++ lib/protos/engine.rb
@@ -4,7 +4,5 @@ module Protos
   class Engine < ::Rails::Engine
     # DOCS: This is the engine for the Protos gem. It allows autoloading the
     # lib when used inside a Rails app.
-
-    config.autoload_paths << Pathname.new(__dir__).join("..")
   end
 end

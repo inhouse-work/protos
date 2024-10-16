6be1901e69595ebc1b83e14bfcfaa07011978d49
Nolan J Tait <nolanjtait@gmail.com>
2024-10-15 18:56:53 -0700

    Adds beta version of phlex 2.0

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ gem "benchmark-memory"
 gem "capybara"
 gem "debug", platforms: %i[mri mingw x64_mingw], require: "debug/prelude"
 gem "memory_profiler"
-gem "phlex-testing-capybara"
 gem "rake"
 gem "rspec"
 gem "rubocop-inhouse", require: false
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require "capybara/rspec"
-require "phlex/testing/capybara"
+require "phlex/testing"
 
 RSpec.configure do |config|
-  config.include Phlex::Testing::Capybara::ViewHelper
+  config.include Phlex::Testing::Capybara
 end

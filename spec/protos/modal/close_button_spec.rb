68a86e65d5aeb0ac0005124ddb5180432e9be3e7
Nolan J Tait <nolanjtait@gmail.com>
2024-10-19 20:02:25 -0700

    Improves spec for modal close button

--- spec/protos/modal/close_button_spec.rb
+++ spec/protos/modal/close_button_spec.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe Protos::Modal::CloseButton do
+  subject { page }
+
   before do
     render described_class.new { "Close" }
   end
 
-  it "renders the dialog form" do
-    expect(page).to have_css("form[method='dialog']")
-  end
-
-  it "renders a close button" do
-    expect(page).to have_button
-    expect(page).to have_content("Close")
-  end
+  it { is_expected.to have_css("form[method='dialog']") }
+  it { is_expected.to have_button }
+  it { is_expected.to have_content("Close") }
+  it { is_expected.to have_css("button[data-action='protos--modal#close']") }
 end

b2ad7958beb8696e33bd1d1eb3c91c2907d40f51
Nolan J Tait <nolanjtait@gmail.com>
2025-06-20 18:51:20 -0700

    Adds docs for Collapse component and its children

--- lib/protos/collapse.rb
+++ lib/protos/collapse.rb
@@ -1,10 +1,35 @@
 # frozen_string_literal: true
 
 module Protos
+  # A collapsible container that can be expanded or collapsed. The title
+  # is visible at all times, and the content is only visible when expanded.
+  #
+  # @see https://daisyui.com/components/collapse/
+  #
+  # @example Basic collapse with checkbox
+  #   Protos::Collapse.new(icon: :plus) do |collapse|
+  #     collapse.title { "Click to expand" }
+  #     collapse.content do
+  #       p { "This content is hidden until expanded." }
+  #     end
+  #   end
+  #
+  # @example Radio button collapse group
+  #   Protos::Collapse.new(input_type: :radio, input_name: "accordion") do |collapse|
+  #     collapse.title { "Section 1" }
+  #     collapse.content { "Content for section 1" }
+  #   end
+  #   Protos::Collapse.new(input_type: :radio, input_name: "accordion") do |collapse|
+  #     collapse.title { "Section 2" }
+  #     collapse.content { "Content for section 2" }
+  #   end
+  #
+  # @example Collapse with initial state
+  #   Protos::Collapse.new(state: :open, icon: :arrow) do |collapse|
+  #     collapse.title { "Already expanded" }
+  #     collapse.content { "This starts open" }
+  #   end
   class Collapse < Component
-    # DOCS: A collapsible container that can be expanded or collapsed. The title
-    # is visible at all times, and the content is only visible when expanded.
-    # https://daisyui.com/components/collapse/
 
     autoload :Title, "protos/collapse/title"
     autoload :Content, "protos/collapse/content"
@@ -29,17 +54,32 @@ module Protos
       :checkbox
     )
 
+    # @!attribute [r] state
+    #   @return [Symbol] One of `:default`, `:open`, or `:close` for initial state.
     option :state, type: States, default: -> { :default }, reader: false
+
+    # @!attribute [r] icon
+    #   @return [Symbol] One of `:arrow` or `:plus` for the collapse icon style.
     option :icon, type: Icons, default: -> { :arrow }, reader: false
+
+    # @!attribute [r] input_type
+    #   @return [Symbol] One of `:radio` or `:checkbox` for the input mechanism.
     option :input_type,
       type: InputTypes,
       default: -> { :checkbox },
       reader: false
+
+    # @!attribute [r] input_name
+    #   @return [String, Integer] Name/ID for the input element. Auto-generated if not provided.
     option :input_name,
       reader: false,
       default: -> { "collapse-#{SecureRandom.hex(4)}" },
       type: Types::String | Types::Integer
 
+    # Renders the collapse container element.
+    #
+    # @yield The content block to define title and content.
+    # @return [nil] outputs to the @buffer
     def view_template
       div(**attrs) do
         if @input_type
@@ -58,8 +98,16 @@ module Protos
       end
     end
 
+    # Renders the collapse title (always visible, used to toggle).
+    #
+    # @param (see Protos::Collapse::Title#initialize)
+    # @return [nil]
     def title(*, **, &) = render Title.new(*, input_id: @input_name, **, &)
 
+    # Renders the collapse content (hidden until expanded).
+    #
+    # @param (see Protos::Collapse::Content#initialize)
+    # @return [nil]
     def content(...) = render Content.new(...)
 
     private
--- lib/protos/collapse/title.rb
+++ lib/protos/collapse/title.rb
@@ -2,15 +2,21 @@
 
 module Protos
   class Collapse
+    # The title of a collapse. This is the content that is always
+    # visible and is used to toggle the collapse.
     class Title < Component
-      # DOCS: The title of a collapse. This is the content that is always
-      # visible and is used to toggle the collapse.
 
+      # @!attribute [r] input_id
+      #   @return [String, Integer, nil] The ID of the associated input element for toggling.
       option :input_id,
         type: Types::String | Types::Integer | Types::Nil,
         reader: false,
         default: -> { }
 
+      # Renders the collapse title element.
+      #
+      # @yield The content block for the title content.
+      # @return [nil] outputs to the @buffer
       def view_template(&)
         if @input_id
           label(for: @input_id.to_s, **attrs, &)

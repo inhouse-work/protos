181a337e92e44003fcf8c87bda3b63a3043a5c92
Nolan J Tait <nolanjtait@gmail.com>
2025-06-20 18:34:10 -0700

    Adds docs for Status component

--- lib/protos/status.rb
+++ lib/protos/status.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 module Protos
+  # A status component
+  #
+  # @example
+  #   Protos::Status.new(type: :success, size: :lg) { "Active" }
+  #
+  # @example With different types
+  #   Protos::Status.new(type: :error) { "Error" }
+  #   Protos::Status.new(type: :warning) { "Warning" }
+  #   Protos::Status.new(type: :info) { "Info" }
   class Status < Component
     STYLES = {
       default: "",
@@ -24,9 +33,18 @@ module Protos
       xl: "status-xl"
     }.freeze
 
+    # @!attribute [r] type
+    #   @return [Symbol] One of `:default`, `:accent`, `:error`, `:info`, `:neutral`, `:primary`, `:secondary`, `:success`, or `:warning` for styling.
     option :type, type: Types::Styles, default: -> { :primary }
+
+    # @!attribute [r] size
+    #   @return [Symbol] One of `:xs`, `:sm`, `:md`, `:lg`, or `:xl` for size.
     option :size, type: Sizes, default: -> { :md }
 
+    # Renders the status element.
+    #
+    # @yield The content block for the status text.
+    # @return [nil] outputs to the @buffer
     def view_template(&)
       div(**attrs, &)
     end

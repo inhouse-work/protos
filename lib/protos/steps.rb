f59d569dceb9ae728c4ffb1bb566252dcbe82417
Nolan J Tait <nolanjtait@gmail.com>
2025-06-20 18:32:59 -0700

    Adds docs for Steps component

--- lib/protos/steps.rb
+++ lib/protos/steps.rb
@@ -1,18 +1,36 @@
 # frozen_string_literal: true
 
 module Protos
+  # A steps component
+  #
+  # @see https://daisyui.com/components/steps/
+  #
+  # @example
+  #   Protos::Steps.new(vertical: true) do |steps|
+  #     steps.step(type: :primary) { "Step 1" }
+  #     steps.step(type: :primary) { "Step 2" }
+  #     steps.step { "Step 3" }
+  #   end
   class Steps < Component
-    # DOCS: Steps can be used to show a list of steps in a process.
-    # https://daisyui.com/components/steps/
 
     autoload :Step, "protos/steps/step"
 
+    # @!attribute [r] vertical
+    #   @return [Boolean] Whether to display steps vertically instead of horizontally.
     option :vertical, type: Types::Bool, default: -> { false }
 
+    # Renders the full steps element.
+    #
+    # @yield The content block to define individual steps.
+    # @return [nil] outputs to the @buffer
     def view_template(&)
       ol(**attrs, &)
     end
 
+    # Renders a step.
+    #
+    # @param (see Protos::Steps::Step#initialize)
+    # @return [nil]
     def step(...) = render Step.new(...)
 
     private

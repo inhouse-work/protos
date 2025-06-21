857bf1d4be1fcccf1ceedfee987d056254951585
Nolan J Tait <nolanjtait@gmail.com>
2025-06-20 18:52:25 -0700

    Adds docs for chat bubble component

--- lib/protos/chat_bubble/content.rb
+++ lib/protos/chat_bubble/content.rb
@@ -2,9 +2,9 @@
 
 module Protos
   class ChatBubble
+    # The main content of a chat bubble. This would normally be the text
+    # content of the message. It will be colored according to the type.
     class Content < Component
-      # DOCS: The main content of a chat bubble. This would normally be the text
-      # content of the message. It will be colored according to the type.
 
       STYLES = {
         default: "",
@@ -18,11 +18,17 @@ module Protos
         warning: "chat-bubble-warning"
       }.freeze
 
+      # @!attribute [r] type
+      #   @return [Symbol] One of `:default`, `:accent`, `:error`, `:info`, `:neutral`, `:primary`, `:secondary`, `:success`, or `:warning` for styling.
       option :type,
         default: -> { :default },
         reader: false,
         type: Types::Styles
 
+      # Renders the chat bubble content element.
+      #
+      # @yield The content block for the message text.
+      # @return [nil] outputs to the @buffer
       def view_template(&)
         div(**attrs, &)
       end

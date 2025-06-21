1f79b4d9c8fa62d19807380abc6e9b9032870991
Nolan J Tait <nolanjtait@gmail.com>
2025-06-20 18:57:45 -0700

    Adds docs for Breadcrumbs component

--- lib/protos/breadcrumbs.rb
+++ lib/protos/breadcrumbs.rb
@@ -1,18 +1,41 @@
 # frozen_string_literal: true
 
 module Protos
+  # A breadcrumbs component for navigation hierarchy
+  #
+  # @see https://daisyui.com/components/breadcrumbs/
+  #
+  # @example Basic breadcrumbs
+  #   Protos::Breadcrumbs.new do |breadcrumbs|
+  #     breadcrumbs.crumb { a(href: "/") { "Home" } }
+  #     breadcrumbs.crumb { a(href: "/products") { "Products" } }
+  #     breadcrumbs.crumb { "Current Page" }
+  #   end
+  #
+  # @example Breadcrumbs with custom separator
+  #   Protos::Breadcrumbs.new(class: "text-sm") do |breadcrumbs|
+  #     breadcrumbs.crumb { a(href: "/") { "Home" } }
+  #     breadcrumbs.crumb { ">" }
+  #     breadcrumbs.crumb { a(href: "/products") { "Products" } }
+  #   end
   class Breadcrumbs < Component
-    # DOCS: A list of breadcrumbs (e.g links) for navigation
-    # https://daisyui.com/components/breadcrumbs/
 
     autoload :Crumb, "protos/breadcrumbs/crumb"
 
+    # Renders the breadcrumbs container element.
+    #
+    # @yield The content block to define breadcrumb items.
+    # @return [nil] outputs to the @buffer
     def view_template(&block)
       nav(**attrs) do
         ul(class: css[:list], &block)
       end
     end
 
+    # Renders a breadcrumb item.
+    #
+    # @param (see Protos::Breadcrumbs::Crumb#initialize)
+    # @return [nil]
     def crumb(...) = render Crumb.new(...)
 
     private

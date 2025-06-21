c8c6e4754139acb6e82c069a4ab84c678d63f9de
Nolan J Tait <nolanjtait@gmail.com>
2025-06-20 19:10:58 -0700

    Add comprehensive documentation for Protos::Component base class

--- lib/protos/component.rb
+++ lib/protos/component.rb
@@ -1,11 +1,42 @@
 # frozen_string_literal: true
 
 module Protos
+  # The base component class that all Protos components inherit from.
+  #
+  # Provides core functionality for:
+  # - Theme-based styling system
+  # - HTML attribute management
+  # - Dry::Initializer for component options
+  # - Phlex::HTML integration for view templates
+  #
+  # @example Creating a custom component
+  #   class MyComponent < Protos::Component
+  #     option :title
+  #     
+  #     def view_template
+  #       div(class: css[:container]) do
+  #         h1 { title }
+  #       end
+  #     end
+  #
+  #     private
+  #
+  #     def theme
+  #       {
+  #         container: "bg-blue-500 text-white p-4"
+  #       }
+  #     end
+  #   end
+  #
+  # @note Key features:
+  #   - Automatically merges theme classes with overrides
+  #   - Handles HTML attributes safely
+  #   - Supports component options via Dry::Initializer
+  #   - Integrates with Phlex for declarative HTML
+  #
+  # @see Protos::Theme The theme system used for styling
+  # @see Protos::Attributes The attribute management system
   class Component < Phlex::HTML
-    # DOCS: Base component for all Protos::Components. You can inherit from this
-    # class to gain flexible components you can style from the outside using css
-    # slots, default attrs and themes.
-
     extend Dry::Initializer[undefined: false]
 
     # Theme can override the css hash and add additional styles

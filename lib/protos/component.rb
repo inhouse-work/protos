ff7a30d6e5556f7eae9a511ba578515c1f2ea933
Nolan J Tait <nolanjtait@gmail.com>
2025-10-10 18:10:15 -0700

    Adds #with_attrs and #with_theme

--- lib/protos/component.rb
+++ lib/protos/component.rb
@@ -52,16 +52,50 @@ module Protos
       @html_options = kwargs.slice(*extra_keys)
     end
 
+    # Allows overriding the theme of an instance after initialization.
+    # This is useful for `superform` where you hand some kind of form control
+    # the input:
+    #
+    # @example
+    # class FormControl < ApplicationForm
+    #   param :input_component
+    #
+    #   def view_template
+    #     render input_component.with_theme(container: "text-red-500")
+    #   end
+    # end
+    #
+    # Because the `input_component` is an instance, there is no easy way to
+    # change the theme without `with_theme`.
+    #
+    # @param overrides [Hash] Theme overrides to merge
+    # @return [self] Returns self for chaining
+    def with_theme(overrides = {})
+      @theme_override = @theme_override.merge(overrides)
+      self
+    end
+
+    # @see #with_theme
+    # @param overrides [Hash] HTML attribute overrides to merge
+    # @return [self] Returns self for chaining
+    def with_attrs(overrides = {})
+      @html_options = @html_options.merge(overrides)
+      self
+    end
+
     private
 
+    # Merged attributes like `data-controller`, `aria-*`, `id`, etc.
     def attrs
       @attrs ||= build_attrs
     end
 
+    # Merged css classes based on the theme and overrides
     def css
       @css ||= build_theme
     end
 
+    # Build the final attributes from all the overriding parts
     def build_attrs(...)
       Attributes
         .new(...)
@@ -70,6 +104,7 @@ module Protos
         .merge(class: css[:container])
     end
 
+    # Build the final theme from all the overriding parts
     def build_theme(...)
       Theme
         .new(...)
--- spec/protos/component_spec.rb
+++ spec/protos/component_spec.rb
@@ -5,77 +5,109 @@ RSpec.describe Protos::Component do
     stub_const(
       "BaseComponent", Class.new(Protos::Component) do
         option :some_property, default: -> { "default" }
-      end
-    )
 
-    stub_const(
-      "TestComponent", Class.new(BaseComponent) do
-        def view_template(&)
-          div(**attrs) do
-            div(class: "#{css[:inner]} #{css[:overridable]}") do
-              span(class: css[:non_existent]) { "Hello" }
-            end
-          end
+        def view_template
+          h1(**attrs) { "hello" }
         end
 
         private
 
-        def default_attrs
-          {
-            data: {
-              "test-component": "true",
-              controller: "controller-one"
-            }
-          }
-        end
-
         def theme
           {
-            container: "test-component removed-component",
-            inner: "test-component-inner",
-            overridable: "removed-component"
+            container: "base-component"
           }
         end
       end
     )
-
-    render TestComponent.new(
-      class: "injected-class",
-      role: "test",
-      data: {
-        value: "test",
-        controller: "controller-two"
-      },
-      theme: {
-        "!container": "removed-component",
-        overridable!: "added-component"
-      }
-    ) { "Hello" }
   end
 
-  it "renders the component" do
-    expect(page).to have_css("div > div > span")
-    expect(page).to have_content("Hello")
+  describe "#with_theme" do
+    it "returns a new instance with the merged theme" do
+      render BaseComponent.new.with_theme(container: "new-class")
+
+      expect(page).to have_css(".base-component.new-class")
+    end
   end
 
-  it "overrides the theme" do
-    expect(page).to have_css(".added-component")
+  describe "#with_attrs" do
+    it "returns a new instance with the merged attributes" do
+      render BaseComponent.new.with_attrs(id: "test-id", data: { role: "test" })
+
+      expect(page).to have_css("h1#test-id[data-role='test']")
+    end
   end
 
-  it "removes classes from the theme" do
-    expect(page).to have_no_css(".removed-component")
-  end
+  context "with an inherited class" do
+    before do
+      stub_const(
+        "TestComponent", Class.new(BaseComponent) do
+          def view_template(&)
+            div(**attrs) do
+              div(class: "#{css[:inner]} #{css[:overridable]}") do
+                span(class: css[:non_existent]) { "Hello" }
+              end
+            end
+          end
 
-  it "applies the styles" do
-    expect(page).to have_css(".test-component")
-    expect(page).to have_css(".injected-class")
-    expect(page).to have_css(".test-component-inner")
-  end
+          private
 
-  it "applies the html options" do
-    expect(page).to have_css("[role='test']")
-    expect(page).to have_css("[data-test-component='true']")
-    expect(page).to have_css("[data-value='test']")
-    expect(page).to have_css("[data-controller='controller-one controller-two']")
+          def default_attrs
+            {
+              data: {
+                "test-component": "true",
+                controller: "controller-one"
+              }
+            }
+          end
+
+          def theme
+            {
+              container: "test-component removed-component",
+              inner: "test-component-inner",
+              overridable: "removed-component"
+            }
+          end
+        end
+      )
+
+      render TestComponent.new(
+        class: "injected-class",
+        role: "test",
+        data: {
+          value: "test",
+          controller: "controller-two"
+        },
+        theme: {
+          "!container": "removed-component",
+          overridable!: "added-component"
+        }
+      ) { "Hello" }
+    end
+
+    it "renders the component" do
+      expect(page).to have_css("div > div > span")
+      expect(page).to have_content("Hello")
+    end
+
+    it "overrides the theme" do
+      expect(page).to have_css(".added-component")
+    end
+
+    it "removes classes from the theme" do
+      expect(page).to have_no_css(".removed-component")
+    end
+
+    it "applies the styles" do
+      expect(page).to have_css(".test-component")
+      expect(page).to have_css(".injected-class")
+      expect(page).to have_css(".test-component-inner")
+    end
+
+    it "applies the html options" do
+      expect(page).to have_css("[role='test']")
+      expect(page).to have_css("[data-test-component='true']")
+      expect(page).to have_css("[data-value='test']")
+      expect(page).to have_css("[data-controller='controller-one controller-two']")
+    end
   end
 end
